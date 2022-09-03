97200ecf3fd85e0e2b954014e1acc22b73fc82d3
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2022-09-03 05:30:49 +0000

    Update gui-verson.py

--- gui-verson.py
+++ gui-verson.py
@@ -1,23 +1,17 @@
 import decimal, json, pyperclip, pyautogui
-end_check = True
-debug_info_start = ""
-debug_info_end = ""
 time = 0.0
-exist = False
 segment = int(pyautogui.prompt("How many Segments are there in the Video?", title="PyTime"))
 for _ in range(segment):
     debug_info_start = pyautogui.prompt("\n(Debug Info) What is the Starting Frame", title="PyTime")
     debug_info_end = pyautogui.prompt("\n(Debug Info) What is the Ending Frame", title="PyTime")
-    #Atleast JSON parsing is here now
+    #json parsing
     debug_info_start_dict = json.loads(debug_info_start)
     debug_info_end_dict = json.loads(debug_info_end)
-    cmt_start = debug_info_start_dict['cmt']
-    cmt_end = debug_info_end_dict['cmt']
-    #todo Make this shit accurate :)
-    time = (decimal.Decimal(cmt_end) - decimal.Decimal(cmt_start)) + decimal.Decimal(time)
-#formats the time
+    #finds time using CMT
+    time = (decimal.Decimal(debug_info_end_dict['cmt']) - decimal.Decimal(debug_info_start_dict['cmt'])) + decimal.Decimal(time)
 time = str(time)
 time = time.split(".", 1)
+#time formatting
 if len(time) > 0:
     seconds = time[0]
     milliseconds = time[1]
@@ -42,6 +36,6 @@ def seconds_to_time(seconds):
     else:
         return (f"{hours}:{minutes}:{seconds}.{milliseconds}")
 formatted_time = seconds_to_time(seconds)
-#prints the time
+#copies mod not and tells time with an alert
 pyperclip.copy(f"Mod Note: Retimed to [b]{formatted_time}[/b] using [url=https://github.com/ConnerConnerConner/PyTime]PyTime[/url]")
 pyautogui.alert(f"Final Time: {formatted_time} \nMod Note copied to Clipboard", title="PyTime")
