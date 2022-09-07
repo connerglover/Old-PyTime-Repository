a51d780449cdfc8040fe32d1ee847896b0ca12f9
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2022-09-06 21:54:15 -0500

    Made GUI and Renamed CLI Version
    Made GUI Version

--- cli-version.py
+++ cli-version.py
@@ -1,15 +1,14 @@
-import curses, pyperclip, json
+import curses
+import pyperclip
+import json
 from time import sleep
-from decimal import Decimal
+from decimal import Decimal as d
 from curses import wrapper
 from curses.textpad import Textbox, rectangle
 #something for curses idk
 def main(stdscr):
-    time = 0
-    menu_current = 0
-    in_menu = True
     #Prints Logo for 2.5 seconds
-    logo = """
+    logo = '''
 
                                  d8,                     
                           d8P   `8P                      
@@ -22,7 +21,7 @@ def main(stdscr):
      d88            ,d8P                                  
      ?8P         `?888P'                                  
 
-    """
+    '''
     stdscr.clear()
     stdscr.addstr(6, 0, logo, curses.A_BOLD)
     stdscr.refresh()
@@ -32,7 +31,7 @@ def main(stdscr):
     seg_box = Textbox(seg_win)
     rectangle(stdscr, 2, 1, 4, 10)
     #Asks for number of segments
-    stdscr.addstr(1, 1, "How many Segments are there in this video?", curses.A_BOLD)
+    stdscr.addstr(1, 1, 'How many Segments are there in this video?', curses.A_BOLD)
     stdscr.refresh()
     seg_box.edit()
     segment_count = seg_box.gather()
@@ -41,32 +40,36 @@ def main(stdscr):
     #Uses the number of segments to repeat debug info collection
     for i in range(segment_count):
         #Asks for debug info and stores the JSON in a variable to be parsed later
-        stdscr.addstr(1, 1, "Copy the Debug Info of the Starting Frame", curses.A_BOLD)
-        stdscr.addstr(2, 1, "Once this is done Press Any Key", curses.A_BOLD)
+        stdscr.addstr(1, 1, 'Copy the Debug Info of the Starting Frame', curses.A_BOLD)
+        stdscr.addstr(2, 1, 'Once this is done Press Any Key', curses.A_BOLD)
         stdscr.refresh()
         stdscr.getch()
         #hijacks the clipboard to make it easier to copy debug info
         debug_info_start = pyperclip.paste()
         stdscr.clear()
         #copies debug info and stores the JSON in a variable to be parsed later again
-        stdscr.addstr(1, 1, "Copy the Debug Info of the Ending Frame", curses.A_BOLD)
-        stdscr.addstr(2, 1, "Once this is done Press Any Key", curses.A_BOLD)
+        stdscr.addstr(1, 1, 'Copy the Debug Info of the Ending Frame', curses.A_BOLD)
+        stdscr.addstr(2, 1, 'Once this is done Press Any Key', curses.A_BOLD)
         stdscr.getch()
         #hijacks the clipboard to make it easier to copy debug info again
         debug_info_end = pyperclip.paste()
         stdscr.refresh()
         stdscr.clear()
         #parses the previously sorted JSON
+        
         debug_info_start_dict = json.loads(debug_info_start)
         debug_info_end_dict = json.loads(debug_info_end)
         #calculates the time difference between the start and end of the segment
-        time = (Decimal(debug_info_end_dict['cmt']) - Decimal(debug_info_start_dict['cmt'])) + Decimal(time)
+        if 'time' in globals():
+            time = (d(debug_info_end_dict['cmt']) - d(debug_info_start_dict['cmt'])) + d(time)
+        else:
+            time = (d(debug_info_end_dict['cmt']) - d(debug_info_start_dict['cmt']))
     #prints menu
-    stdscr.addstr(1, 2, "[1] Just Time")
-    stdscr.addstr(2, 2, "[2] Mod Note")
+    stdscr.addstr(1, 2, '[1] Just Time')
+    stdscr.addstr(2, 2, '[2] Mod Note')
     stdscr.refresh()
     time = str(time)
-    time = time.split(".", 1)
+    time = time.split('.', 1)
     #formats the time to be more than seconds and milliseconds
     if len(time) > 0:
         #takes the milliseconds and the time and stores them in different strvariables
@@ -81,51 +84,53 @@ def main(stdscr):
         seconds = str(seconds)
         minutes = str(minutes)
         hours = str(hours)
-        if seconds == "0":
-            return ("0." + milliseconds)
-        elif minutes == "0":
+        if seconds == '0':
+            return ('0.' + milliseconds)
+        elif minutes == '0':
             if len(seconds) == 1:
-                return (f"0{seconds}s {milliseconds}ms")
+                return (f'0{seconds}s {milliseconds}ms')
             else:
-                return (f"{seconds}s {milliseconds}ms")
-        elif hours == "0":
-            return (f"{minutes}m {seconds}s {milliseconds}ms")
+                return (f'{seconds}s {milliseconds}ms')
+        elif hours == '0':
+            return (f'{minutes}m {seconds}s {milliseconds}ms')
         else:
-            return (f"{hours}h {minutes}m {seconds}s {milliseconds}ms")
+            return (f'{hours}h {minutes}m {seconds}s {milliseconds}ms')
     formatted_time = format_time(seconds)
+    if not 'in_menu' in globals():
+        in_menu = True
     while in_menu:
         menu_select = stdscr.getkey()
         #menu selection
-        if menu_select == "1":
+        if menu_select == '1':
             menu_current = 1
             stdscr.clear()
-            stdscr.addstr(1, 2, "[1] Just Time", curses.A_REVERSE)
-            stdscr.addstr(2, 2, "[2] Mod Note")
+            stdscr.addstr(1, 2, '[1] Just Time', curses.A_REVERSE)
+            stdscr.addstr(2, 2, '[2] Mod Note')
             stdscr.refresh()
-        elif menu_select == "2":
+        elif menu_select == '2':
             menu_current = 2
             stdscr.clear()
-            stdscr.addstr(1, 2, "[1] Just Time")
-            stdscr.addstr(2, 2, "[2] Mod Note", curses.A_REVERSE)
+            stdscr.addstr(1, 2, '[1] Just Time')
+            stdscr.addstr(2, 2, '[2] Mod Note', curses.A_REVERSE)
             stdscr.refresh()
-        elif menu_select == "q":
-            if menu_current == 0:
+        elif menu_select == 'q':
+            if not 'menu_current' in globals():
                 continue
             elif menu_current == 1:
                 #prints just the time
                 in_menu = False
                 stdscr.clear()
-                stdscr.addstr(1, 1, f"The Final Time is {formatted_time}", curses.A_BOLD)
-                stdscr.addstr(2, 1, "Press Any Key to Exit", curses.A_BOLD)
+                stdscr.addstr(1, 1, f'The Final Time is {formatted_time}', curses.A_BOLD)
+                stdscr.addstr(2, 1, 'Press Any Key to Exit', curses.A_BOLD)
                 stdscr.refresh()
             elif menu_current == 2:
                 #prints the mod note and copies it to clipboard
                 in_menu = False
                 stdscr.clear()
-                pyperclip.copy(f"Mod Note: Retimed to [b]{formatted_time}[/b] using [url=https://github.com/ConnerConnerConner/PyTime]PyTime[/url]")
-                stdscr.addstr(1, 1, f"Mod Note: Retimed to {formatted_time} using PyTime", curses.A_BOLD)
-                stdscr.addstr(2, 1, "Mod Note Has Been Copied to Clipboard", curses.A_BOLD)
-                stdscr.addstr(3, 1, "Press Any Key to Exit", curses.A_BOLD)
+                pyperclip.copy(f'Mod Note: Retimed to [b]{formatted_time}[/b] using [url=https://github.com/ConnerConnerConner/PyTime]PyTime[/url]')
+                stdscr.addstr(1, 1, f'Mod Note: Retimed to {formatted_time} using PyTime', curses.A_BOLD)
+                stdscr.addstr(2, 1, 'Mod Note Has Been Copied to Clipboard', curses.A_BOLD)
+                stdscr.addstr(3, 1, 'Press Any Key to Exit', curses.A_BOLD)
                 stdscr.refresh()
         else:
            continue
--- gui-verson.py
+++ gui-verson.py
@@ -1,41 +1,112 @@
-import decimal, json, pyperclip, pyautogui
-time = 0.0
-segment = int(pyautogui.prompt("How many Segments are there in the Video?", title="PyTime"))
-for _ in range(segment):
-    debug_info_start = pyautogui.prompt("\n(Debug Info) What is the Starting Frame", title="PyTime")
-    debug_info_end = pyautogui.prompt("\n(Debug Info) What is the Ending Frame", title="PyTime")
-    #JSON poarsing
-    debug_info_start_dict = json.loads(debug_info_start)
-    debug_info_end_dict = json.loads(debug_info_end)
-    #Finds time using CMT and previous time
-    time = (decimal.Decimal(debug_info_end_dict['cmt']) - decimal.Decimal(debug_info_start_dict['cmt'])) + decimal.Decimal(time)
-time = str(time)
-time = time.split(".", 1)
-#Time formatting
-if len(time) > 0:
-    seconds = time[0]
-    milliseconds = time[1]
-    milliseconds = str(milliseconds)
-    seconds = int(seconds)
-def seconds_to_time(seconds):
-    seconds = int(seconds)
-    minutes = seconds//60
-    hours = minutes//60
-    seconds = str(seconds)
-    minutes = str(minutes)
-    hours = str(hours)
-    if seconds == "0":
-        return ("0." + milliseconds)
-    elif minutes == "0":
-        if len(seconds) == 1:
-            return (f"0{seconds}.{milliseconds}")
+import json
+import pyperclip
+import PySimpleGUI as sg
+from decimal import Decimal as d
+
+sg.theme('DarkGrey12')
+
+def time_formatting(time):
+    time = str(time)
+    time = time.split('.', 1)
+    if len(time) > 0:
+        seconds = time[0]
+        milliseconds = time[1]
+        milliseconds = str(milliseconds)
+        seconds = int(seconds)
+    def seconds_to_time(seconds):
+        seconds = int(seconds)
+        minutes = seconds//60
+        hours = minutes//60
+        seconds = str(seconds)
+        minutes = str(minutes)
+        hours = str(hours)
+        if seconds == '0':
+            return ('0.' + milliseconds)
+        elif minutes == '0':
+            if len(seconds) == 1:
+                return (f'0{seconds}.{milliseconds}')
+            else:
+                return (f'{seconds}.{milliseconds}')
+        elif hours == '0':
+            return (f'{minutes}:{seconds}.{milliseconds}')
         else:
-            return (f"{seconds}.{milliseconds}")
-    elif hours == "0":
-        return (f"{minutes}:{seconds}.{milliseconds}")
+            return (f'{hours}:{minutes}:{seconds}.{milliseconds}')
+    return seconds_to_time(seconds)
+
+def calculate_load(dbi, dbe):
+    try:
+        dbi_dict = json.loads(dbi)
+    except:
+        sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error')
+        return False
+    try:
+        dbe_dict = json.loads(dbe)
+    except:
+        sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error')
+        return False
+    loads = d(dbe_dict['cmt']) - d(dbi_dict['cmt'])
+    return loads
+
+def calcualte_final(dbi, dbe, loads):
+    try:
+        dbi_dict = json.loads(dbi)
+    except:
+        sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error')
+        return
+    try:
+        dbe_dict = json.loads(dbe)
+    except:
+        sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error')
+        return
+    if 'time' in locals() or 'time' in globals():
+        time = (d(dbe_dict['cmt']) - d(dbi_dict['cmt'])) + d(time)
     else:
-        return (f"{hours}:{minutes}:{seconds}.{milliseconds}")
-formatted_time = seconds_to_time(seconds)
-#Copies mod note to clipboard and prints time with a pyautogui alert
-pyperclip.copy(f"Mod Note: Retimed to [b]{formatted_time}[/b] using [url=https://github.com/ConnerConnerConner/PyTime]PyTime[/url]")
-pyautogui.alert(f"Final Time: {formatted_time} \nMod Note copied to Clipboard", title="PyTime")
+        time = (d(dbe_dict['cmt']) - d(dbi_dict['cmt']))
+    time_loads = time
+    time = time - loads
+    no_loads = time_formatting(time)
+    with_loads = time_formatting(time_loads)
+    if loads == 0:
+        sg.popup(f'Without Loads: {no_loads}', 'Mod Note Copied to Clipboard', title = 'Results')
+    else:
+        sg.popup(f'Without Loads: {no_loads}, With Loads: {with_loads}', 'Mod Note Copied to Clipboard', title = 'Results')
+    pyperclip.copy(f'Mod Note: Retimed to [b]{no_loads}[/b] using [url = https://github.com/ConnerConnerConner/PyTime]PyTime[/url]')
+    return
+
+main_layout = [
+    [sg.Text('PyTime', font = ('Helvetica', 36))],
+    [sg.InputText(key = 'dbi', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info Start', font = ('Helvetica', 16), justification = 'right')],
+    [sg.InputText(key = 'dbe', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info End', font = ('Helvetica', 16), justification = 'right')],
+    [sg.InputText(key = 'dbi_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info Start (Loads)', font = ('Helvetica', 14), justification = 'right')],
+    [sg.InputText(key = 'dbe_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info End (Loads)', font = ('Helvetica', 14), justification = 'right')],
+    [sg.Button('Calculate', font = ('Helvetica', 16)), sg.Button('Add Loads', font = ('Helvetica', 16))]
+]
+
+load_layout = [
+    [sg.Text('Add Loads', font = ('Helvetica', 18))],
+    [sg.InputText(key = 'dbi_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info Start', font = ('Helvetica', 14), justification = 'right')],
+    [sg.InputText(key = 'dbe_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info End', font = ('Helvetica', 14), justification = 'right')],
+    [sg.Button('Add Loads', font = ('Helvetica', 14)), sg.Exit(font = ('Helvetica', 14))],
+]
+
+main_window = sg.Window('PyTime', main_layout, resizable = False, element_justification = 'left')
+
+while True:
+    event, values = main_window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    if event == 'Add Loads':
+        dbil = values['dbi_loads']
+        dbel = values['dbe_loads']
+        if not 'loads' in globals():
+            loads = calculate_load(dbil, dbel)
+        else:
+            loads = calculate_load(dbil, dbel) + loads
+    if event == 'Calculate':
+        dbi = values['dbi']
+        dbe = values['dbe']
+        if not 'loads' in globals():
+            loads = 0
+        calcualte_final(dbi, dbe, loads)
+
+main_window.close()
