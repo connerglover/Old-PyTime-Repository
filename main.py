c4fb4859f488bdaaebf58bacc84e09ab0a2d1f9b
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2022-09-08 21:28:21 -0500

    fixed a bunch of issues and redid format

--- main.py
+++ main.py
@@ -2,98 +2,124 @@ import json
 import pyperclip
 import PySimpleGUI as sg
 from decimal import Decimal as d
+import random
 
+# Sets the theme for the GUI
 sg.theme('DarkGrey12')
 
+#Used to calculate the time between 2 times
 class timer:
     def format(time):
+        #changes time into a string and splits the string into an array between seconds and milliseconds
         time = str(time)
         time = time.split('.', 1)
-        if len(time) > 0:
-            seconds = time[0]
-            milliseconds = time[1]
-            milliseconds = str(milliseconds)
-            seconds = int(seconds)
-        def seconds_to_time(seconds):
-            seconds = int(seconds)
-            minutes = seconds//60
-            hours = minutes//60
-            if seconds > 59:
-                seconds = seconds - 60
-            seconds = str(seconds)
-            minutes = str(minutes)
-            hours = str(hours)
-            if seconds == '0':
-                return ('0.' + milliseconds)
-            elif minutes == '0':
-                if len(seconds) == 1:
-                    return (f'0{seconds}.{milliseconds}')
-                else:
-                    return (f'{seconds}.{milliseconds}')
-            elif hours == '0':
-                return (f'{minutes}:{seconds}.{milliseconds}')
+        seconds = time[0]
+        milliseconds = time[1]
+        milliseconds = str(milliseconds)
+        seconds = int(seconds)
+        minutes = seconds//60
+        hours = minutes//60
+        if seconds > 60:
+            seconds = seconds - 60
+        seconds = str(seconds)
+        minutes = str(minutes)
+        hours = str(hours)
+        if seconds == '0':
+            return (f'0.{milliseconds}')
+        elif minutes == '0':
+            if len(seconds) == 1:
+                return (f'0{seconds}.{milliseconds}')
             else:
-                return (f'{hours}:{minutes}:{seconds}.{milliseconds}')
-        return seconds_to_time(seconds)
+                return (f'{seconds}.{milliseconds}')
+        elif hours == '0':
+            return (f'{minutes}:{seconds}.{milliseconds}')
+        else:
+            return (f'{hours}:{minutes}:{seconds}.{milliseconds}')
+        
 
-    def load(dbi, dbe):
+    def load(dbi_end, dbi_start):
         try:
-            dbi_dict = json.loads(dbi)
+            dbis_dict = json.loads(dbi_start)
         except:
             sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error')
-            return False
+            return
         try:
-            dbe_dict = json.loads(dbe)
+            dbie_dict = json.loads(dbi_end)
         except:
             sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error')
-            return False
-        loads = d(dbe_dict['cmt']) - d(dbi_dict['cmt'])
-        main_window['dbi_loads'].update('')
-        main_window['dbe_loads'].update('')
+            return
+        try:
+            cmt_end = dbie_dict['cmt']
+            cmt_start = dbis_dict['cmt']
+        except:
+            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
+        try:
+            time = (d(cmt_end) - d(cmt_start)) + d(time)
+        except:
+            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
+            return
+        if -abs(time) == time:
+            sg.popup('Error', 'The end is greater than the start.', title = 'Error')
+            return
+        main_window['dbis_loads'].update('')
+        main_window['dbie_loads'].update('')
         sg.popup(f'Loads Added', title = 'Loads', font = ('Helvetica', 16))
         return loads
 
-    def final(dbi, dbe, loads):
+    def final(dbi_start, dbi_end, loads):
         try:
-            dbi_dict = json.loads(dbi)
+            dbis_dict = json.loads(dbi_start)
         except:
             sg.popup('Error (Start)', 'Debug Info is not valid.', title = 'Error')
             return
         try:
-            dbe_dict = json.loads(dbe)
+            dbie_dict = json.loads(dbi_end)
         except:
             sg.popup('Error (End)', 'Debug Info is not valid.', title = 'Error')
             return
-        if 'time' in locals() or 'time' in globals():
-            time = (d(dbe_dict['cmt']) - d(dbi_dict['cmt'])) + d(time)
-        else:
-            time = (d(dbe_dict['cmt']) - d(dbi_dict['cmt']))
+        try:
+            cmt_end = dbie_dict['cmt']
+            cmt_start = dbis_dict['cmt']
+        except:
+            sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
+        time = (d(cmt_end) - d(cmt_start))
+        if -abs(time) == time:
+            sg.popup('Error', 'The end is greater than the start.', title = 'Error')
+            return
         time_loads = time
         time = time - loads
-        no_loads = format(time)
-        with_loads = format(time_loads)
+        no_loads = timer.format(time)
+        with_loads = timer.format(time_loads)
         if loads == 0:
             final_confirm = sg.popup_yes_no(f'Without Loads: {no_loads}', 'Would you like the Mod Note to be Copied to the Clipboard?', title = 'Results')
             if final_confirm == 'Yes':
-                pyperclip.copy(f'Mod Note: Retimed to [b]{no_loads}[/b] using [url = https://github.com/ConnerConnerConner/PyTime]PyTime[/url]')
+                pyperclip.copy(f'Mod Note: Retimed to {no_loads} https://github.com/ConnerConnerConner/PyTime')
             elif final_confirm == 'No':
                return 
         else:
             final_confirm = sg.popup_yes_no(f'Without Loads: {no_loads}, With Loads: {with_loads}', 'Mod Note Copied to Clipboard', title = 'Results')
         if final_confirm == 'Yes':
-            pyperclip.copy(f'Mod Note: Retimed to [b]{no_loads}[/b] using [url = https://github.com/ConnerConnerConner/PyTime]PyTime[/url]')
+            pyperclip.copy(f'Mod Note: Retimed to {no_loads} using https://github.com/ConnerConnerConner/PyTime')
         elif final_confirm == 'No':
              return
-
-main_layout = [
-    [sg.Text('PyTime', font = ('Helvetica', 36))],
-    [sg.InputText(key = 'dbi', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info Start', font = ('Helvetica', 16), justification = 'right')],
-    [sg.InputText(key = 'dbe', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info End', font = ('Helvetica', 16), justification = 'right')],
-    [sg.InputText(key = 'dbi_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info Start (Loads)', font = ('Helvetica', 14), justification = 'right')],
-    [sg.InputText(key = 'dbe_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info End (Loads)', font = ('Helvetica', 14), justification = 'right')],
-    [sg.Button('Calculate', font = ('Helvetica', 16)), sg.Button('Add Loads', font = ('Helvetica', 16)), sg.Button('Remove All Loads', font = ('Helvetica', 16))]
-]
-
+if (random.randint(1, 1000) == 69):
+    main_layout = [
+        [sg.Text('conner solos rekto', font = ('Helvetica', 36))],
+        [sg.InputText(key = 'dbis', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info Start', font = ('Helvetica', 16), justification = 'right')],
+        [sg.InputText(key = 'dbie', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info End', font = ('Helvetica', 16), justification = 'right')],
+        [sg.InputText(key = 'dbis_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info Start (Loads)', font = ('Helvetica', 14), justification = 'right')],
+        [sg.InputText(key = 'dbie_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info End (Loads)', font = ('Helvetica', 14), justification = 'right')],
+        [sg.Button('Calculate', font = ('Helvetica', 16)), sg.Button('Add Loads', font = ('Helvetica', 16)), sg.Button('Remove All Loads', font = ('Helvetica', 16))]
+    ]
+else:
+    main_layout = [
+        [sg.Text('PyTime', font = ('Helvetica', 36))],
+        [sg.InputText(key = 'dbis', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info Start', font = ('Helvetica', 16), justification = 'right')],
+        [sg.InputText(key = 'dbie', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info End', font = ('Helvetica', 16), justification = 'right')],
+        [sg.InputText(key = 'dbis_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info Start (Loads)', font = ('Helvetica', 14), justification = 'right')],
+        [sg.InputText(key = 'dbie_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info End (Loads)', font = ('Helvetica', 14), justification = 'right')],
+        [sg.Button('Calculate', font = ('Helvetica', 16)), sg.Button('Add Loads', font = ('Helvetica', 16)), sg.Button('Remove All Loads', font = ('Helvetica', 16))]
+    ]
 main_window = sg.Window('PyTime', main_layout, resizable = False, element_justification = 'left', size=(447, 253),  icon='PyTime.ico')
 
 while True:
@@ -103,23 +129,28 @@ while True:
     if event == 'Remove All Loads':
         lr_confirm = sg.popup_yes_no('Are you sure you want to remove all loads?', title = 'Remove All Loads', font = ('Helvetica', 16))
         if lr_confirm == 'Yes':
-            main_window['dbi_loads'].update('')
-            main_window['dbe_loads'].update('')
+            main_window['dbis_loads'].update('')
+            main_window['dbie_loads'].update('')
             loads = 0
         elif lr_confirm == 'No':
             continue
     if event == 'Add Loads':
-        dbil = values['dbi_loads']
-        dbel = values['dbe_loads']
+        dbis_loads = values['dbis_loads']
+        dbiel_loads = values['dbie_loads']
         if not 'loads' in globals():
-            loads = timer.load(dbil, dbel)
+            loads = timer.load(dbis_loads, dbiel_loads)
         else:
-            loads = timer.load(dbil, dbel) + loads
+            try:
+                loads = timer.load(dbis_loads, dbiel_loads) + loads
+            except:
+                continue
     if event == 'Calculate':
-        dbi = values['dbi']
-        dbe = values['dbe']
+        dbi_start = values['dbis']
+        dbi_end = values['dbie']
         if not 'loads' in globals():
             loads = 0
-        timer.final(dbi, dbe, loads)
+        main_window['dbis'].update('')
+        main_window['dbie'].update('')
+        timer.final(dbi_start, dbi_end, loads)
 
 main_window.close()
