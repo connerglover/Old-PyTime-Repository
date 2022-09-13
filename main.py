8783816a7b3a831510635ebda97ad828c6bf1bea
Conner <110003454+ConnerConnerConner@users.noreply.github.com>
2022-09-12 22:50:16 -0500

    ADDED FRAME ROUNDING, IT'S ACCURATE!

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from json import loads as json_loads
 from pyperclip import copy
 import PySimpleGUI as sg
 from decimal import Decimal as d
-from random import randint
-# Sets the theme for the GUI
+from math import floor as round_down
+
 sg.theme('DarkGrey12')
 
-#Used to calculate the time between 2 times
 class timer:
+    def frame_round(time, fps):
+        output = d(time - time%(d(1)/fps)) #Slush0Puppy's Code BTW 💀
+        return round(output, 3)
+
     def format(time):
-        #changes time into a string and splits the string into an array between seconds and milliseconds
         time = str(time)
         time = time.split('.', 1)
         seconds = time[0]
@@ -20,6 +22,8 @@ class timer:
         hours = minutes//60
         if seconds > 60:
             seconds = seconds - (minutes * 60)
+        if minutes > 60:
+            minutes = minutes - (hours * 60)
         seconds = str(seconds)
         minutes = str(minutes)
         hours = str(hours)
@@ -36,7 +40,7 @@ class timer:
             return (f'{hours}:{minutes}:{seconds}.{milliseconds}')
         
 
-    def load(dbi_end, dbi_start):
+    def load(dbi_end, dbi_start, fps):
         try:
             dbis_dict = json_loads(dbi_start)
         except:
@@ -52,6 +56,8 @@ class timer:
             cmt_start = dbis_dict['cmt']
         except:
             sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
+        cmt_start = timer.frame_round(d(cmt_start), fps)
+        cmt_end = timer.frame_round(d(cmt_end), fps)
         try:
             time = (d(cmt_end) - d(cmt_start)) + d(time)
         except:
@@ -60,12 +66,14 @@ class timer:
         if -abs(time) == time:
             sg.popup('Error', 'The end is greater than the start.', title = 'Error')
             return
+        loads = timer.frame_round(time, fps)
+        print(loads)
         main_window['dbis_loads'].update('')
         main_window['dbie_loads'].update('')
         sg.popup(f'Loads Added', title = 'Loads', font = ('Helvetica', 16))
         return loads
 
-    def final(dbi_start, dbi_end, loads):
+    def final(dbi_start, dbi_end, loads, fps):
         try:
             dbis_dict = json_loads(dbi_start)
         except:
@@ -81,13 +89,14 @@ class timer:
             cmt_start = dbis_dict['cmt']
         except:
             sg.popup('Error (CMT)', 'CMT is not Valid.', title = 'fuck you rekto')
-        time = (d(cmt_end) - d(cmt_start))
-        if -abs(time) == time:
+        cmt_start = timer.frame_round(d(cmt_start), fps)
+        cmt_end = timer.frame_round(d(cmt_end), fps)
+        time_loads = (d(cmt_end) - d(cmt_start))
+        if -abs(time_loads) == time_loads:
             sg.popup('Error', 'The end is greater than the start.', title = 'Error')
             return
-        time_loads = time
-        time = time - loads
-        no_loads = timer.format(time)
+        time_noloads = time_loads - loads
+        no_loads = timer.format(time_noloads)
         with_loads = timer.format(time_loads)
         if loads == 0:
             final_confirm = sg.popup_yes_no(f'Without Loads: {no_loads}', 'Would you like the Mod Note to be Copied to the Clipboard?', title = 'Results')
@@ -101,18 +110,8 @@ class timer:
             copy(f'Mod Note: Retimed to {no_loads} using https://github.com/ConnerConnerConner/PyTime')
         elif final_confirm == 'No':
              return
-if (randint(1, 1000) == 69):
-    main_layout = [
-        [sg.Text('conner solos rekto', font = ('Helvetica', 36))],
-        [sg.InputText(key = 'dbis', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info Start', font = ('Helvetica', 16), justification = 'right')],
-        [sg.InputText(key = 'dbie', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info End', font = ('Helvetica', 16), justification = 'right')],
-        [sg.InputText(key = 'dbis_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info Start (Loads)', font = ('Helvetica', 14), justification = 'right')],
-        [sg.InputText(key = 'dbie_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info End (Loads)', font = ('Helvetica', 14), justification = 'right')],
-        [sg.Button('Calculate', font = ('Helvetica', 16)), sg.Button('Add Loads', font = ('Helvetica', 16)), sg.Button('Remove All Loads', font = ('Helvetica', 16))]
-    ]
-else:
-    main_layout = [
-        [sg.Text('PyTime', font = ('Helvetica', 36))],
+main_layout = [
+        [sg.Text('PyTime', font = ('Helvetica', 36)), sg.Text('   FPS', font = (' Helvetica', 30)), sg.InputText('60', size = (5, 1), key = 'fps', font = ('Helvetica', 30))],
         [sg.InputText(key = 'dbis', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info Start', font = ('Helvetica', 16), justification = 'right')],
         [sg.InputText(key = 'dbie', font = ('Helvetica', 16), pad = ((5, 0), (0, 0)), size = (20, 1)), sg.Text('  Debug Info End', font = ('Helvetica', 16), justification = 'right')],
         [sg.InputText(key = 'dbis_loads', font = ('Helvetica', 14), pad = ((5, 0), (0, 0)), size = (15, 1)), sg.Text('   Debug Info Start (Loads)', font = ('Helvetica', 14), justification = 'right')],
@@ -146,10 +145,12 @@ while True:
     if event == 'Calculate':
         dbi_start = values['dbis']
         dbi_end = values['dbie']
+        fps = values['fps']
+        fps = int(fps)
         if not 'loads' in globals():
             loads = 0
         main_window['dbis'].update('')
         main_window['dbie'].update('')
-        timer.final(dbi_start, dbi_end, loads)
+        timer.final(dbi_start, dbi_end, loads, fps)
 
 main_window.close()
